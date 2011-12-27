d61f43ed3589aee66435b19fd46297410662b9b2
Anthony Campagna <gucommander@gmail.com>
2011-12-27 13:30:06 -0500

    first commit

---
+++ js/common.js
@@ -0,0 +1,177 @@
+$(document).ready(function() {
+	openCreateWorldDialog();
+	
+	$("#grid-tile-type").change(function() {
+		populateTileTypeValues(this);
+	});
+	
+	$("#generate-world").click(function() {
+		generateWorld();
+	});
+});
+
+function openCreateWorldDialog() {
+	$(function() {
+		$( "#dialog-create-world" ).dialog({
+			modal: true,
+			resize: false,
+			buttons: {
+				Submit: function() {
+					$( this ).dialog( "close" );
+					createWorldSubmitCallback();
+				}
+			}
+		});
+	});
+}
+
+function openEditTileDialog(elem) {
+	
+	cur_id = $(elem).data('id');
+	cur_x = $(elem).data('x');
+	cur_y = $(elem).data('y');
+	
+	$("#edit-grid-tile-info-id").html("id: "+cur_id);
+	$("#edit-grid-tile-info-x").html("x: "+cur_x);
+	$("#edit-grid-tile-info-y").html("y: "+cur_y);
+	
+	edge = isTileOnEdge(cur_x,cur_y);
+	populateTileTypes(edge);
+
+	$(function() {
+		$( "#dialog-edit-grid-tile" ).dialog({
+			resize: false,
+			buttons: {
+				Submit: function() {
+					$( this ).dialog( "close" );
+					createEditTileSubmitCallback();
+				}
+			}
+		});
+	});
+}
+
+function createWorldSubmitCallback() {
+	var width = $("#world-width").val();
+	var height = $("#world-height").val();
+
+	$("#world-editor-grid").html(generateGrid(width,height));
+	$("#world-grid").css('width',width*32);
+	$("#world-grid").css('height',height*32);
+	
+	setGridEvents();
+	
+	$("#world-editor-grid").show();
+}
+
+function createEditTileSubmitCallback() {
+	elem = $("#tile-"+cur_id);
+	type = $('#grid-tile-type').val();
+	val = $('#grid-tile-value').val();
+	
+	elem.data('type',type);
+	elem.data('value',val);
+	
+	if(type == "s") {
+		spawns[0] = cur_id;
+	}
+	
+	elem.html(type + ":" + val);
+}
+
+function generateWorld() {
+	//Get Adjacent Waypoint from Spawn
+	turnTileIntoPath(spawns[0]);
+	adj = getAdjacentWaypoint($('#tile-'+spawns[0]).data('x'),$('#tile-'+spawns[0]).data('y'));
+	
+	if(adj) {
+		turnTileIntoPath(adj);
+		goal = getNextWaypoint($('#tile-'+adj).data('x'),$('#tile-'+adj).data('y'),$('#tile-'+adj).data('value'));
+	}
+	
+	exportGridToJavaArray();
+}
+
+function openAlertDialog(title,msg) {
+	$("#dialog-alert").attr('title',title);
+	$("#dialog-alert").html(msg);
+	$(function() {
+		$( "#dialog-alert" ).dialog({
+			resize: false,
+			buttons: {
+				Ok: function() {
+					$( this ).dialog( "close" );
+				}
+			}
+		});
+	});
+}
+
+function generateGrid(width,height) {
+	grid_width = width;
+	grid_height = height;
+
+	html = "<table id=\"world-grid\">";
+	i = 0;
+	
+	for (j=0;j<height;j++) {
+		grid[j] = Array();
+		html += "<tr>";
+		for(k=0;k<width;k++) {
+			i++;
+			grid[j][k] = i;
+			html += "<td style=\"background:url('images/tiles.png') -32px 0;\" class=\"grid-tile\" id=\"tile-"+i+"\" data-id=\""+i+"\" data-x=\""+k+"\" data-y=\""+j+"\" data-type=\"\" data-value=\"\"></td>";
+			
+		}
+		html+= "</tr>";
+	}
+	html += "</table>";
+	
+	return html;
+}
+
+function populateTileTypes(edge) {	
+	elem_html = "<option value=\"\">Set Type</option>\
+		<option value=\"w\">Waypoint</option>\
+		<option value=\"d\">Doodad</option>";
+		
+	if(edge) {
+		elem_html += "<option value=\"s\">Spawn</option>\
+			<option value=\"g\">Goal</option>";
+	}
+	
+	$("#grid-tile-type").html(elem_html);
+	
+}
+
+function populateTileTypeValues(elem) {
+	val = $(elem).val();
+	
+	if(val == "") {
+		$("#grid-tile-value").attr('disabled','');
+		$("#grid-tile-value").html("");
+	}
+	
+	if(val == "s" || val == "g") {
+		$("#grid-tile-value").removeAttr('disabled');
+		$("#grid-tile-value").html("<option value=\"01\">1</option><option value=\"02\">2</option><option value=\"03\">3</option><option value=\"04\">4</option><option value=\"05\">5</option>");
+	}
+	
+	if(val == "w") {
+		$("#grid-tile-value").removeAttr('disabled');
+		$("#grid-tile-value").html("<option value=\"n\">Up</option><option value=\"e\">Right</option><option value=\"s\">Down</option><option value=\"w\">Left</option>");
+	}
+}
+
+function setGridEvents() {
+	$(".grid-tile").mouseenter(function() {
+		$(this).toggleClass('active',0);
+	}).mouseleave(function(){
+		$(this).toggleClass('active',0);
+	});
+	
+	$(".grid-tile").click(function() {
+		openEditTileDialog(this);
+	});
+}
+
---
+++ js/grid.js
@@ -0,0 +1,147 @@
+var grid = Array();
+var spawns = Array();
+var cur_id;
+var cur_x;
+var cur_y;
+var grid_width;
+var grid_height;
+
+function isTileOnEdge(x,y) {
+	if(x == 0 || x == grid_width-1 || y == 0 || y == grid_height-1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function getIdByCoords(x,y) {
+	if(x >= 0 && x<= grid_width-1 && y>=0 && y<= grid_height-1) {
+		return grid[y][x];
+	} else {
+		return false;
+	}
+}
+
+function findAdjacentTiles(x,y) {
+	id = getIdByCoords(x,y);
+	adjTiles = Array();
+	
+	up = getIdByCoords(x,y-1);
+	if(up) {
+		adjTiles[0] = up;
+	}
+	
+	right = getIdByCoords(x+1,y);
+	if(right) {
+		adjTiles[1] = right;
+	}
+	
+	down = getIdByCoords(x,y+1);
+	if(down) {
+		adjTiles[2] = down;
+	}
+	
+	left = getIdByCoords(x-1,y);
+	if(left) {
+		adjTiles[3] = left;
+	}
+	
+	return adjTiles;
+}
+
+function getAdjacentWaypoint(x,y) {
+	adjTiles = findAdjacentTiles(x,y);
+	for (var i = 0; i < adjTiles.length; i++) {
+		if(adjTiles[i]) {
+			if($("#tile-"+adjTiles[i]).data('type') == "w") {
+				return adjTiles[i];
+			}
+		}
+	}
+	return false;
+}
+
+function getNextWaypoint(x,y,dir) {
+	if(dir == "n") {
+		for (var i = y-1; i >= 0; i--) {
+			id = changeTileAndReturnWaypointId(x,i);
+			if(id) {
+				break;
+			}
+		}
+	}
+	if(dir == "s") {
+		for (var i = y+1; i <= grid_height-1; i++) {
+			id = changeTileAndReturnWaypointId(x,i);
+			if(id) {
+				break;
+			}
+		}
+	}
+	if(dir == "w") {
+		for (var i = x-1; i >= 0; i--) {
+			id = changeTileAndReturnWaypointId(i,y);
+			if(id) {
+				break;
+			}
+		}
+	}
+	if(dir == "e") {
+		for (var i = x+1; i <= grid_width-1; i++) {
+			id = changeTileAndReturnWaypointId(i,y);
+			if(id) {
+				break;
+			}
+		}
+	}
+	
+	//alert($('#tile-'+id).data('x')+","+$('#tile-'+id).data('y')+","+$('#tile-'+id).data('value'));
+	
+	if(id) {
+		if($('#tile-'+id).data('type') != "g") {
+			getNextWaypoint($('#tile-'+id).data('x'),$('#tile-'+id).data('y'),$('#tile-'+id).data('value'));
+		} else {
+			return id;
+		}
+	}
+}
+
+function changeTileAndReturnWaypointId(x,y) {
+
+	id = getIdByCoords(x,y);
+	turnTileIntoPath(id);
+	
+	if($('#tile-'+id).data('type') == "w" || $('#tile-'+id).data('type') == "g") {
+		return id;
+	}
+	
+	return false;
+}
+
+function turnTileIntoPath(id) {
+	$('#tile-'+id).css('background','url(\'images/tiles.png\') 0 0');
+}
+
+function exportGridToJavaArray() {
+	exp = "{";
+	i = 0;
+	for(j=0;j<grid_height;j++) {
+		exp += "{";
+		for(k=0;k<grid_width;k++) {
+			i++;
+			if(k==0) {
+				exp += "\""+$('#tile-'+i).html()+"\"";
+			} else {
+				exp += ",\""+$('#tile-'+i).html()+"\"";
+			}
+		}
+		exp += "},";
+	}
+	exp += "}";
+	
+	openAlertDialog("Export",exp);
+}
+
+
+
+
